6f69c56271bf063bf4f12981841ea67204f4fdf2
hyeonga493 <hyeonga4349@naver.com>
2023-10-24 17:37:28 +0900

    hyeonga world!!!

--- src/main/java/tommy/spring/GitTest/App.java
+++ src/main/java/tommy/spring/GitTest/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!~~!!" );
-        System.out.println("ÀüÁö¿¬");
+        System.out.println("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½");
+        System.out.println("Hyeonga world!!!!");
     }
 }
